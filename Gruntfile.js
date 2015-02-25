cb3e8b8669cfbb082da940d19dae6cd16f7ffc50
Hady Osman <hadyos@gmail.com>
2015-02-25 16:28:08 +1300

    Configure mocha for unit testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,23 +2,23 @@ module.exports = function (grunt) {
   'use strict';
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-mocha-test');
 
   grunt.initConfig({
-    mocha: {
-      'options': {
-        'run': false,
-        'logErrors': true,
-        'reporter': 'Dot'
-      },
-      'test': {'src': './test/index.html'}
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: './test/**/*.js'
+      }
     },
     jshint: {
-      'options': {'jshintrc': true},
-      'lint': {'src': ['**/*.js']}
+      options: {jshintrc: true},
+      lint: {src: ['**/*.js']}
     }
   });
 
-  grunt.registerTask('test', ['mocha']);
+  grunt.registerTask('test', ['lint', 'mochaTest']);
   grunt.registerTask('lint', ['jshint']);
 };
---
+++ test/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var assert = require('chai').assert;
+var dalekPluginize = require('../index.js');
+
+describe('dalek-extend', function(){
+
+  it('should exist', function(){
+    assert.ok(dalekPluginize);
+  });
+
+});
